165758a2fc58c6ead5d5db976a8863260d4ace8f
clemens-db <>
2019-10-23 17:30:14 +0000

    sub

--- Notebook-5.py
+++ Notebook-5.py
@@ -1,2 +1,4 @@
 # Databricks notebook source
 print ('hello from notebook 5 from github -- update')
+
+# test
--- Notebook-7.py
+++ Notebook-7.py
@@ -1,2 +1,3 @@
 # Databricks notebook source
 print ('hello from notebook 7 from git')
+# test2
