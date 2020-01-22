3894a0df4925804c1f55443fd81277a6b508d864
clemens-db <>
2020-01-22 23:03:42 +0000

    mods

--- Demo6 notebook.py
+++ Demo6 notebook.py
@@ -1,3 +1,12 @@
 # Databricks notebook source
 # new
 print ('hello from github')
+
+# COMMAND ----------
+
+code = '<form><input type="text" /></form>'
+
+displayHTML(code)
+
+# COMMAND ----------
+
