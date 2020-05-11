4913de11b9d04a8763c9728e203626606cb87439
clemens-db <>
2020-05-11 16:58:56 +0000

    asdf

--- Notebook-10.py
+++ Notebook-10.py
@@ -3,4 +3,8 @@
 
 # COMMAND ----------
 
+<<<<<<< HEAD
 # test 11 from DB
+=======
+# test update from github 10
+>>>>>>> branch 'master' of https://github.com/clemens-db/projects-demo
--- Notebook-7.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-print ('hello from notebook 7 from git')
-# test2
-# update from github
